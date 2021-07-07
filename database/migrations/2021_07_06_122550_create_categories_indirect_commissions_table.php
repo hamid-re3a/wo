f143fcb4639086e066a84cd2f77ed84da06e9487
hamid <hamire3a@gmail.com>
2021-07-07 22:14:42 +0430

    update

---
+++ database/migrations/2021_07_06_122550_create_categories_indirect_commissions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesIndirectCommissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories_indirect_commissions', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('category_id');
+
+            $table->integer('level');
+            $table->integer('percentage');
+
+            $table->unique(['category_id', 'level']);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories_indirect_commissions');
+    }
+}

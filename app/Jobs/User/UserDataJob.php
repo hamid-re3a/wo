1fc2b5b76a00cbbaac221b0cef5a7ba9fa7fa8d2
Dariush Molaie <d.molaie@fanappayment.ir>
2021-08-09 14:57:11 +0430

    add config RabitMq and merge by master

---
+++ app/Jobs/User/UserDataJob.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Jobs\User;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class UserDataJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $data;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $data_final = json_encode($this->data['name']);
+        echo "event has been handle. the name of userData is:".$data_final. PHP_EOL;
+
+    }
+}

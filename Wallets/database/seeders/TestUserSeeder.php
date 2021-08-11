0e2b9e27fbdee1b5c47687df9d366a766192f472
NimaN2D <nima.nouri.d@gmail.com>
2021-08-11 16:05:24 +0430

    Seed test users if environment is local

--- Wallets/database/seeders/TestUserSeeder.php
+++ Wallets/database/seeders/TestUserSeeder.php
@@ -8,21 +8,26 @@ class TestUserSeeder extends Seeder
 {
     public function run()
     {
-        WalletUser::create([
-            'user_id' => 1,
-            'first_name' => 'Nima',
-            'last_name' => 'Nouri',
-            'username' => 'niamn2d',
-            'email' => 'nima.nouri.d@gmail.com'
-        ]);
+        // Load local seeder
+        if (app()->environment() === 'local')
+        {
+            WalletUser::create([
+                'user_id' => 1,
+                'first_name' => 'Nima',
+                'last_name' => 'Nouri',
+                'username' => 'niamn2d',
+                'email' => 'nima.nouri.d@gmail.com'
+            ]);
 
-        WalletUser::create([
-            'user_id' => 2,
-            'first_name' => 'Ali',
-            'last_name' => 'Gholami',
-            'username' => 'Gholami',
-            'email' => 'ali@gmail.com'
-        ]);
+            WalletUser::create([
+                'user_id' => 2,
+                'first_name' => 'Ali',
+                'last_name' => 'Gholami',
+                'username' => 'Gholami',
+                'email' => 'ali@gmail.com'
+            ]);
+        }
+
     }
 
 }

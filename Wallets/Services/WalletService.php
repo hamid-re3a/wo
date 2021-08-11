018e309df9f292ac73e929ee77f84a9fe00e1cfa
NimaN2D <nima.nouri.d@gmail.com>
2021-08-11 20:39:58 +0430

    WalletService for gRPC

--- Wallets/Services/WalletService.php
+++ Wallets/Services/WalletService.php
@@ -4,28 +4,146 @@
 namespace Wallets\Services;
 
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Packages\Services\UserService;
 use User\Services;
 
-class WalletService implements  WalletServiceInterface
+class WalletService implements WalletServiceInterface
 {
+    private $depositWallet;
+    private $earningWallet;
+
+    public function __construct()
+    {
+        $this->depositWallet = config('depositWallet');
+        $this->earningWallet = config('earningWallet');
+    }
+
+    private function walletUser($user)
+    {
+        return UserService::getUser([
+            'user_id' => $user->getId(),
+            'first_name' => $user->getFirstName(),
+            'last_name' => $user->getLastName(),
+            'email' => $user->getEmail(),
+            'username' => $user->getUsername()
+        ]);
+    }
 
     public function deposit(Deposit $deposit): Transaction
     {
-        // TODO: Implement deposit() method.
+        try {
+            DB::beginTransaction();
+            $walletUser = $this->walletUser($deposit->getUser());
+
+            $transaction = $deposit->getTransaction();
+            if (
+                $transaction->getConfiremd() AND
+                $transaction->getAmount() > 0 AND
+                $transaction->getToWalletName() AND
+                $transaction->getToUserId() AND
+                $transaction->getConfiremd() AND
+                in_array(strtolower($transaction->getToWalletName()) ,['deposit','deposit wallet'])
+            ) {
+                $bankService = new BankService($walletUser);
+
+                $bankService->deposit($this->depositWallet,$transaction->getAmount(), $transaction->getDescription() ?: null);
+
+                $transactionObject = new Transaction();
+                $transactionObject->setConfiremd(true);
+
+                DB::commit();
+                return $transactionObject;
+            } else {
+                DB::rollBack();
+                return new Transaction();
+            }
+        } catch (\Throwable $exception) {
+            return new Transaction();
+        }
     }
 
     public function withdraw(Withdraw $withdraw): Transaction
     {
-        // TODO: Implement withdraw() method.
+        try {
+            DB::beginTransaction();
+            $walletUser = $this->walletUser($withdraw->getUser());
+
+            $transaction = $withdraw->getTransaction();
+            if (
+                $transaction->getConfiremd() AND
+                $transaction->getAmount() > 0 AND
+                $transaction->getToWalletName() AND
+                $transaction->getToUserId() AND
+                $transaction->getConfiremd() AND
+                in_array(strtolower($transaction->getToWalletName()) ,['earning','earning wallet'])
+            ) {
+                $bankService = new BankService($walletUser);
+
+                $bankService->withdraw($this->earningWallet,$transaction->getAmount(), $transaction->getDescription() ?: null);
+
+                $transactionObject = new Transaction();
+                $transactionObject->setConfiremd(true);
+
+                DB::commit();
+                return $transactionObject;
+            } else {
+                DB::rollBack();
+                return new Transaction();
+            }
+        } catch (\Throwable $exception) {
+            DB::rollBack();
+            return new Transaction();
+        }
     }
 
     public function transfer(Transfer $transfer): Transaction
     {
-        // TODO: Implement transfer() method.
+        try {
+            DB::beginTransaction();
+            $transaction = $transfer->getTransaction();
+            if (
+                $transaction->getConfiremd() AND
+                $transaction->getAmount() > 0 AND
+                $transaction->getToWalletName() AND
+                $transaction->getToUserId() AND
+                $transaction->getConfiremd()
+            ) {
+                $fromUser = $this->walletUser($transfer->getFromUser());
+                $toUser = $this->walletUser($transfer->getToUser());
+                $fromBankService = new BankService($fromUser);
+                $toBankService = new BankService($toUser);
+
+                $fromWallet = Str::contains(Str::lower($transaction->getFromWalletName()), 'deposit') ? $this->depositWallet : $this->earningWallet;
+                $toWallet = Str::contains(Str::lower($transaction->getToWalletName()), 'deposit') ? $this->depositWallet : $this->earningWallet;
+
+                $fromBankService->transfer($fromWallet,$toBankService->getWallet($toWallet), $transaction->getAmount(), $transaction->getDescription() ?: null);
+                DB::commit();
+            } else {
+                DB::rollBack();
+                return new Transaction();
+            }
+        } catch (\Throwable $exception) {
+
+            DB::rollBack();
+            return new Transaction();
+        }
     }
 
-    public function getBalance(Services\User $user): Wallet
+    public function getBalance(Wallet $wallet): Wallet
     {
-        // TODO: Implement getBalance() method.
+        try {
+            $walletUser = $this->walletUser($wallet->getUser());
+            $bankService = new BankService($walletUser);
+            $balance = $bankService->getBalance(Str::contains(Str::lower($wallet->getName()),'deposit') ? $this->depositWallet : $this->earningWallet);
+
+            $response = new Wallet();
+            $response->setBalance($balance);
+            return $response;
+
+        } catch (\Throwable $exception) {
+            return new Wallet();
+        }
     }
 }

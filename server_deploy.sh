e902fd2c7751a3a538cff99ec2733be113dcb7a7
ridetothefuture <84563812+ridetothefuture@users.noreply.github.com>
2021-08-23 14:37:40 +0400

    Update server_deploy.sh

--- server_deploy.sh
+++ server_deploy.sh
@@ -19,7 +19,7 @@ rm -rf temp-project/
 cp -u .env.staging  .env
 composer dumpautoload
 composer config --global --auth http-basic.ride-to-the-future.repo.repman.io token 67001fefcf70038c817987b7431f2d17498dc5c2409b4748e51cad87a69b8567
-composer update
+composer install
 
 # Update codebas
 #chmod 777 .* -R

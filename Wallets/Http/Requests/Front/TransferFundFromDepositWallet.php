a96b6deca894147bef4e17a9d6d1b530b84c2c0a
NimaN2D <nima.nouri.d@gmail.com>
2021-08-31 18:26:23 +0430

    [Wallet] Minimum & Maximum transfer fund

--- Wallets/Http/Requests/Front/TransferFundFromDepositWallet.php
+++ Wallets/Http/Requests/Front/TransferFundFromDepositWallet.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class TransferFundFromDepositWallet extends FormRequest
 {
+    private $minimum_amount;
+    private $maximum_amount;
 
     /**
      * Determine if the user is authorized to make this request.
@@ -24,11 +26,21 @@ class TransferFundFromDepositWallet extends FormRequest
      */
     public function rules()
     {
+        $this->minimum_amount = walletGetSetting('minimum_transfer_fund_amount');
+        $this->maximum_amount = walletGetSetting('maximum_transfer_fund_amount');
         return [
             'member_id' => 'required|integer|exists:users,member_id',
-            'amount' => 'required|integer|min:1'
+            'amount' => "required|integer|min:{$this->minimum_amount}|max:{$this->maximum_amount}"
         ];
 
+    }
+
+    public function messages()
+    {
+        return [
+            'amount.min' => "Minimum allowed transfer is {$this->minimum_amount} PF",
+            'amount.max' => "Maximum allowed transfer is {$this->maximum_amount} PF",
+        ];
     }
 
 }

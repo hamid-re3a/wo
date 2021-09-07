55713cac36acdfd90069d5c8050f9ae41f4ac3a1
NimaN2D <nima.nouri.d@gmail.com>
2021-09-07 19:02:53 +0430

    UpdateUserJob to APIGateway when user invoice has been expired

--- app/Jobs/User/UpdateUserJob.php
+++ app/Jobs/User/UpdateUserJob.php
@@ -7,17 +7,16 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use User\Services\User;
 
 class UpdateUserJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    private $user;
+    private $data;
 
-    public function __construct(User $user)
+    public function __construct($data)
     {
-        $this->user = $user;
+        $this->data = $data;
     }
 
     /**

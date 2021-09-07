846f8d59d543c4bb07d035dc6d8c3be68df0054b
NimaN2D <nima.nouri.d@gmail.com>
2021-09-07 17:38:03 +0430

    [Order] Wallet & Giftcode for order package

---
+++ Orders/Http/Requests/Front/Order/OrderRequestWithGiftcode.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Orders\Http\Requests\Front\Order;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Orders\Services\OrderService;
+use Payments\Services\PaymentCurrency;
+
+class OrderRequestWithGiftcode extends FormRequest
+{
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'package_id' => 'required|numeric',
+            'plan' => array('in:' . implode(',', ORDER_PLANS)),
+            'giftcode' => 'required|string'
+        ];
+    }
+
+    /**
+     * get name of payment currency
+     * @return array
+     */
+    private function getNamePaymentCurrency()
+    {
+        $payment_currencies = app(OrderService::class)->getPaymentCurrencies()->getPaymentCurrencies();
+        $payment_currency_name_array = array();
+        foreach ($payment_currencies as $payment_currency) {
+            $payment_currency_name_array[] = $payment_currency->getName();
+        }
+        return $payment_currency_name_array;
+    }
+
+    /**
+     * get name payment type
+     * @return array
+     */
+    private function getNamePaymentType()
+    {
+        $payment_types = app(OrderService::class)->getPaymentTypes()->getPaymentTypes();
+        $payment_type_name_array = array();
+        foreach ($payment_types as $payment_type) {
+            $payment_type_name_array[] = $payment_type->getName();
+        }
+        return $payment_type_name_array;
+    }
+
+
+}
---
+++ Orders/resources/lang/en/order.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'responses' => [
+        'something-went-wrong' => 'Oops, Something went wrong, Try again please.',
+
+        'giftcode' => [
+            'wrong-code' => 'Invalid giftcode .',
+            'expired' => 'Entered giftcode has been expired .',
+            'used' => 'Giftcode is not usable .',
+            'wrong-package' => 'Giftcode is not usable .',
+        ],
+
+        'wallet' => [
+            'not-enough-balance' => 'Insufficient balance',
+        ]
+    ],
+];
+

5bc548e3a09f367793fb4a16687e3c5ff75188f9
NimaN2D <nima.nouri.d@gmail.com>
2021-09-07 11:57:00 +0430

    Scribe URL

--- config/scribe.php
+++ config/scribe.php
@@ -307,7 +307,7 @@ INTRO
     /*
      * The base URL to be used in examples. If this is empty, Scribe will use the value of config('app.url').
      */
-    'base_url' => 'https://staging-subscription.janex.org',
+    'base_url' => 'https://staging-api-gateway.janex.org/api/gateway/subscription',
 
     /*
      * Generate a Postman collection (v2.1.0) in addition to HTML docs.

f17939547833ae0eaf28fb994ddab2da2488be3d
NimaN2D <nima.nouri.d@gmail.com>
2021-09-07 14:21:51 +0430

    Wallet jobs

---
+++ app/Jobs/Wallet/WalletDepositDataJob.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Jobs\User;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+use User\Services\UserService;
+use Wallets\Services\Deposit;
+use Wallets\Services\WalletService;
+
+class WalletDepositDataJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $data;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Log::info('WalletDepositDataJob handled');
+
+    }
+}

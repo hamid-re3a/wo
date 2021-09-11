b7f053707e2d28e94bbec8e63b5d237bd6ae4941
NimaN2D <nima.nouri.d@gmail.com>
2021-09-11 19:29:15 +0430

    Improvements

--- Giftcode/Models/Giftcode.php
+++ Giftcode/Models/Giftcode.php
@@ -73,6 +73,11 @@ class Giftcode extends Model
     /**
      * Relation
      */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
     public function creator()
     {
         return $this->belongsTo(User::class, 'user_id', 'id');

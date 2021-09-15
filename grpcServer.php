ffc52f9d3b4a487a3f3354f070964cb28d7221bf
NimaN2D <nima.nouri.d@gmail.com>
2021-09-15 14:58:45 +0430

    GRPC

--- grpcServer.php
+++ grpcServer.php
@@ -1,16 +1,17 @@
 <?php
-require getcwd() . '/vendor/autoload.php';
-require getcwd() .' /public/index.php';
-
-$grpc = new \Mix\Grpc\Server();
-$grpc->register(\Wallets\Services\WalletGrpcService::class);
-
-Swoole\Coroutine\run(function () use ($grpc) {
-    $server = new Swoole\Coroutine\Http\Server('0.0.0.0', 9596, false);
-    $server->handle('/', $grpc->handler());
-    $server->set([
-        'open_http2_protocol' => true,
-        'http_compression' => false,
-    ]);
-    $server->start();
-});
+echo __DIR__;
+//require getcwd() . '/vendor/autoload.php';
+//require getcwd() . '/public/index.php';
+//
+//$grpc = new \Mix\Grpc\Server();
+//$grpc->register(\Wallets\Services\WalletGrpcService::class);
+//
+//Swoole\Coroutine\run(function () use ($grpc) {
+//    $server = new Swoole\Coroutine\Http\Server('0.0.0.0', 9596, false);
+//    $server->handle('/', $grpc->handler());
+//    $server->set([
+//        'open_http2_protocol' => true,
+//        'http_compression' => false,
+//    ]);
+//    $server->start();
+//});

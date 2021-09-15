1a067ef7e7cce2259398feeb85d52c4f16053afb
kia <mohammadrezakiani98@gmail.com>
2021-09-15 15:06:16 +0430

    Comment composer dumpautoload

--- server_deploy.sh
+++ server_deploy.sh
@@ -18,7 +18,7 @@ shopt -s dotglob
 rsync -rv --exclude=.git temp-project/* .
 rm -rf temp-project/
 cp -u .env.staging  .env
-composer dumpautoload
+#composer dumpautoload
 composer config --global --auth http-basic.ride-to-the-future.repo.repman.io token 67001fefcf70038c817987b7431f2d17498dc5c2409b4748e51cad87a69b8567
 composer update
 

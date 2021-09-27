1acdd1c99b81024b05f360ab2ce35bc5a3a6de12
NimaN2D <nima.nouri.d@gmail.com>
2021-09-27 20:22:10 +0330

    Payment Bug fix

--- Payments/Services/Processors/PaymentProcessor.php
+++ Payments/Services/Processors/PaymentProcessor.php
@@ -28,7 +28,7 @@ class PaymentProcessor
             'payable_id' => $invoice_request->getPayableId(),
             'payable_type' => $invoice_request->getPayableType(),
             'pf_amount' => $invoice_request->getPfAmount(),
-            'amount' => pfToUsd($invoice_request->getPfAmount()),
+            'amount' => $invoice_request->getAmount(),
             'due_amount' => $invoice_request->getDueAmount(),
             'paid_amount' => $invoice_request->getPaidAmount(),
             'transaction_id' => $invoice_request->getTransactionId(),

32d3d6536074daecaba57e3f9d632845640757e1
NimaN2D <nima.nouri.d@gmail.com>
2021-10-03 17:52:06 +0330

    TestCases improvements

--- Payments/routes/api.php
+++ Payments/routes/api.php
@@ -10,7 +10,8 @@ use Payments\Http\Controllers\Front\PaymentCurrencyController;
 use Payments\Http\Controllers\Front\PaymentTypeController;
 use Payments\Http\Controllers\Front\WebhookController;
 
-Route::middleware(['auth', 'role:super-admin|subscriptions-payment-admin'])->prefix('admin')->name('admin.')->group(function () {
+Route::name('payments.')->group(function(){
+    Route::middleware(['auth', 'role:super-admin|subscriptions-payment-admin'])->prefix('admin')->name('admin.')->group(function () {
 
 
         Route::name('type.')->prefix("type")->group(function () {
@@ -39,23 +40,24 @@ Route::middleware(['auth', 'role:super-admin|subscriptions-payment-admin'])->pre
             Route::patch('update', [EmailContentController::class, 'update'])->name('update');
         });
 
-});
-
-Route::middleware(['role:client'])->group(function () {
-    Route::name('invoices.')->prefix("invoices")->group(function () {
-        Route::post('cancel_invoice', [InvoiceController::class, 'cancelInvoice'])->name('cancel_invoice');
-        Route::get('/check-pending-order-invoice', [InvoiceController::class, 'pendingOrderInvoice'])->name('check');
-        Route::get('/check-pending-wallet-invoice', [InvoiceController::class, 'pendingWalletInvoice'])->name('pendingWalletInvoice');
-        Route::get('/', [InvoiceController::class, 'index'])->name('get-list');
-        Route::post('/', [InvoiceController::class, 'show'])->name('get-invoice-details');
-        Route::post('transactions', [InvoiceController::class, 'transactions'])->name('transactions');
-    });
-    Route::name('currency.')->prefix("currency")->group(function () {
-        Route::get('', [PaymentCurrencyController::class, 'index'])->name('index');
     });
 
-    Route::name('type.')->prefix("type")->group(function () {
-        Route::get('', [PaymentTypeController::class, 'index'])->name('index');
+    Route::middleware(['role:client'])->group(function () {
+        Route::name('invoices.')->prefix("invoices")->group(function () {
+            Route::post('cancel_invoice', [InvoiceController::class, 'cancelInvoice'])->name('cancel_invoice');
+            Route::get('/check-pending-order-invoice', [InvoiceController::class, 'pendingOrderInvoice'])->name('check');
+            Route::get('/check-pending-wallet-invoice', [InvoiceController::class, 'pendingWalletInvoice'])->name('pendingWalletInvoice');
+            Route::get('/', [InvoiceController::class, 'index'])->name('get-list');
+            Route::post('/', [InvoiceController::class, 'show'])->name('get-invoice-details');
+            Route::post('transactions', [InvoiceController::class, 'transactions'])->name('transactions');
+        });
+        Route::name('currency.')->prefix("currency")->group(function () {
+            Route::get('', [PaymentCurrencyController::class, 'index'])->name('index');
+        });
+
+        Route::name('type.')->prefix("type")->group(function () {
+            Route::get('', [PaymentTypeController::class, 'index'])->name('index');
+        });
     });
 });
 

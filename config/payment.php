de1015ee0585c2f5a0002754eb9a5c11180ec8f2
hamid <hamire3a@gmail.com>
2021-10-11 12:52:28 +0330

    value env

--- config/payment.php
+++ config/payment.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'btc-pay-server-domain' => 'https://testnet.demo.btcpayserver.org/',
-    'btc-pay-server-store-id' => 'BbPfXmDoY2jhaCAkz2fxxYfnNgkRLCv2Agp1W1BNHUhE',
-    'btc-pay-server-api-token' => 'token 2ce510aee96c5fa134622dd8adf95acb9f87c171',
+    'btc-pay-server-domain' => env('BTC_PAY_SERVER_DOMAIN','https://testnet.demo.btcpayserver.org/'),
+    'btc-pay-server-store-id' => env('BTC_PAY_SERVER_STORE_ID','BbPfXmDoY2jhaCAkz2fxxYfnNgkRLCv2Agp1W1BNHUhE'),
+    'btc-pay-server-api-token' => env('BTC_PAY_SERVER_API_TOKEN','token 2ce510aee96c5fa134622dd8adf95acb9f87c171'),
 
     'payment_types' => [
         [

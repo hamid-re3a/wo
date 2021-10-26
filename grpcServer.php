aa497b4408f3f2a23ca09a77660b156b613fabcf
hamid <hamire3a@gmail.com>
2021-10-26 17:43:27 +0330

    add grpc order

--- grpcServer.php
+++ grpcServer.php
@@ -1,12 +1,14 @@
 <?php
 //require '/home/stagingapigateway/public_html/API-Gateway/vendor/autoload.php';
 //require '/home/stagingapigateway/public_html/API-Gateway/public/index.php';
+
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/public/index.php';
 
 $grpc = new \Mix\Grpc\Server();
 $grpc->register(\Wallets\Services\WalletGrpcService::class);
 $grpc->register(\Packages\Services\Grpc\PackageGrpcService::class);
+$grpc->register(\Orders\Services\Grpc\OrderGrpcService::class);
 
 Swoole\Coroutine\run(function () use ($grpc) {
     $server = new Swoole\Coroutine\Http\Server('0.0.0.0', 9596, false);

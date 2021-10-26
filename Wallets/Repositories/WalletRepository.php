129391f678cd33bc2964245e907fd5726e0c3ca4
NimaN2D <nima.nouri.d@gmail.com>
2021-10-26 20:23:03 +0330

    [Wallet] float number

--- Wallets/Repositories/WalletRepository.php
+++ Wallets/Repositories/WalletRepository.php
@@ -70,7 +70,7 @@ class WalletRepository
                 $sum_query->where('payable_id', '=', $user_id);
 
             $results[$key] =
-                $sum_query->whereHas('metaData', function (Builder $subQuery) use ($type) {
+                (float) $sum_query->whereHas('metaData', function (Builder $subQuery) use ($type) {
                     $subQuery->where('wallet_transaction_meta_data.type_id', '=', $type->id);
                 })->sum('amount') / 100;
 

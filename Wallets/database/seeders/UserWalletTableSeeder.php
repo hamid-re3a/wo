670a89acf7b3364e9cb7f0a2c4d8778f2de8bfaa
NimaN2D <nima.nouri.d@gmail.com>
2021-10-31 18:30:21 +0330

    Composer update for bavix wallet

--- Wallets/database/seeders/UserWalletTableSeeder.php
+++ Wallets/database/seeders/UserWalletTableSeeder.php
@@ -22,27 +22,27 @@ class UserWalletTableSeeder extends Seeder
         $bankService = new BankService($user);
         $bankService->getWallet(WALLET_NAME_DEPOSIT_WALLET);
         $bankService->getWallet(WALLET_NAME_EARNING_WALLET);
-//        $bankService->deposit(WALLET_NAME_DEPOSIT_WALLET,10000000);
-//
-//        /**
-//         *
-//         */
-//        $user = User::query()->firstOrCreate(['id' => 2]);
-//        $bankService = new BankService($user);
-//        $bankService->getWallet(WALLET_NAME_DEPOSIT_WALLET);
-//        $bankService->getWallet(WALLET_NAME_EARNING_WALLET);
-//        $bankService->deposit(WALLET_NAME_DEPOSIT_WALLET,10000000000);
-//        $bankService->deposit(WALLET_NAME_EARNING_WALLET,10000000000);
-//
-//        /**
-//         *
-//         */
-//        $customer_user = User::query()->firstOrCreate(['id' => 3]);
-//        $bankService = new BankService($customer_user);
-//        $bankService->getWallet(WALLET_NAME_DEPOSIT_WALLET);
-//        $bankService->getWallet(WALLET_NAME_EARNING_WALLET);
-//        $bankService->deposit(WALLET_NAME_DEPOSIT_WALLET,10000000000);
-//        $bankService->deposit(WALLET_NAME_EARNING_WALLET,10000000000);
+        $bankService->deposit(WALLET_NAME_DEPOSIT_WALLET,10000000);
+
+        /**
+         *
+         */
+        $user = User::query()->firstOrCreate(['id' => 2]);
+        $bankService = new BankService($user);
+        $bankService->getWallet(WALLET_NAME_DEPOSIT_WALLET);
+        $bankService->getWallet(WALLET_NAME_EARNING_WALLET);
+        $bankService->deposit(WALLET_NAME_DEPOSIT_WALLET,10000000000);
+        $bankService->deposit(WALLET_NAME_EARNING_WALLET,10000000000);
+
+        /**
+         *
+         */
+        $customer_user = User::query()->firstOrCreate(['id' => 3]);
+        $bankService = new BankService($customer_user);
+        $bankService->getWallet(WALLET_NAME_DEPOSIT_WALLET);
+        $bankService->getWallet(WALLET_NAME_EARNING_WALLET);
+        $bankService->deposit(WALLET_NAME_DEPOSIT_WALLET,10000000000);
+        $bankService->deposit(WALLET_NAME_EARNING_WALLET,10000000000);
 
 
 

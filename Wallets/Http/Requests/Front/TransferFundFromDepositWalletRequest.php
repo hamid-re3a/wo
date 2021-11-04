be69d08f90940a6b6cebee63a53bbfac1f000384
NimaN2D <nima.nouri.d@gmail.com>
2021-11-04 19:35:58 +0330

    Request improvements

--- Wallets/Http/Requests/Front/TransferFundFromDepositWalletRequest.php
+++ Wallets/Http/Requests/Front/TransferFundFromDepositWalletRequest.php
@@ -12,6 +12,13 @@ class TransferFundFromDepositWalletRequest extends FormRequest
     private $maximum_amount;
     private $member_id;
     private $wallet_balance;
+    private $fee;
+
+    public function __construct(array $query = [], array $request = [], array $attributes = [], array $cookies = [], array $files = [], array $server = [], $content = null)
+    {
+        parent::__construct($query, $request, $attributes, $cookies, $files, $server, $content);
+        $this->prepare();
+    }
 
     /**
      * Determine if the user is authorized to make this request.
@@ -31,16 +38,19 @@ class TransferFundFromDepositWalletRequest extends FormRequest
      */
     public function rules()
     {
-        $this->prepare();
-        $this->minimum_amount = getWalletSetting('minimum_transfer_fund_amount');
-        $this->maximum_amount = getWalletSetting('maximum_transfer_fund_amount');
-        list($total, $fee) = $this->request->has('amount') ? calculateTransferFee($this->request->get('amount')) : [0,0];
-        $this->request->set('amount_new', (double) $this->request->get('amount'));
-        $this->request->set('amount', (double) $this->request->get('amount') + $fee);
 
         return [
             'member_id' => 'required|integer|exists:users,member_id|not_in:' . $this->member_id,
-            'amount' => "required|numeric|min:{$this->minimum_amount}|max:{$this->maximum_amount}|lte:{$this->wallet_balance}",
+            'amount' => [
+                'required',
+                'numeric',
+                'min:' . $this->minimum_amount,
+                'max:' . $this->maximum_amount,
+                function($attribute,$value,$fail){
+                    if(($value + $this->fee) > $this->wallet_balance)
+                        return $fail('Insufficient amount .');
+                }
+            ],
         ];
 
     }
@@ -52,7 +62,6 @@ class TransferFundFromDepositWalletRequest extends FormRequest
             'amount.max' => 'Maximum allowed transfer is ' . formatCurrencyFormat($this->maximum_amount) ." PF .",
             'member_id.not_in' => 'You are not allowed transfer PF to your account .',
             'member_id.exists' => 'Invalid membership ID .',
-            'amount.lte' => 'Insufficient amount'
         ];
     }
 
@@ -66,6 +75,11 @@ class TransferFundFromDepositWalletRequest extends FormRequest
             $this->member_id = $user->member_id;
             $bank_service = new BankService($user);
             $this->wallet_balance = $bank_service->getBalance(WALLET_NAME_DEPOSIT_WALLET);
+
+            $this->minimum_amount = getWalletSetting('minimum_transfer_fund_amount');
+            $this->maximum_amount = getWalletSetting('maximum_transfer_fund_amount');
+            list($total, $fee) = $this->request->has('amount') ? calculateTransferFee($this->request->get('amount')) : [0,0];
+            $this->fee = $fee;
         }
     }
 

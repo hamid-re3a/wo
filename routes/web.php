d7bebcde00d0b9da53bb8644e9b7e23bc571558d
NimaN2D <nima.nouri.d@gmail.com>
2021-11-16 15:21:37 +0330

    Health check for AWS

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ use Illuminate\Support\Facades\Route;
 //});
 
 Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
-Route::get('/_healthz', [\App\Http\Controllers\HealthCheckController::class,'_healthz']);
+Route::get('_healthz', [\App\Http\Controllers\HealthCheckController::class,'_healthz']);

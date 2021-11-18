b1045a872fc836df918074575461fdb6ce92616f
hamid <hamire3a@gmail.com>
2021-11-18 10:33:22 +0330

    convert pacakge fix

--- Wallets/database/seeders/EmailContentSeeder.php
+++ Wallets/database/seeders/EmailContentSeeder.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Wallets\database\seeders;
 
 use Illuminate\Support\Facades\DB;
@@ -9,32 +10,34 @@ class EmailContentSeeder extends Seeder
 {
     public function run()
     {
-        if(defined('WALLET_EMAIL_CONTENTS') AND is_array(WALLET_EMAIL_CONTENTS)) {
-            $emails = [];
-            $now = now()->toDateTimeString();
-            foreach(WALLET_EMAIL_CONTENTS AS $key => $email) {
+        if (defined('WALLET_EMAIL_CONTENTS') AND is_array(WALLET_EMAIL_CONTENTS)) {
+            if (EmailContent::query()->count() == 0) {
+                $emails = [];
+                $now = now()->toDateTimeString();
+                foreach (WALLET_EMAIL_CONTENTS AS $key => $email) {
 
-                if(filter_var(env('MAIL_FROM', $email['from']), FILTER_VALIDATE_EMAIL))
-                    $from =  env('MAIL_FROM', $email['from']);
-                else
-                    $from = $email['from'];
+                    if (filter_var(env('MAIL_FROM', $email['from']), FILTER_VALIDATE_EMAIL))
+                        $from = env('MAIL_FROM', $email['from']);
+                    else
+                        $from = $email['from'];
 
-                $emails[] = [
-                    'key' => $key,
-                    'is_active' => $email['is_active'],
-                    'subject' => $email['subject'],
-                    'from' => env('MAIL_FROM',$from),
-                    'from_name' => $email['from_name'],
-                    'body' => $email['body'],
-                    'variables' => $email['variables'],
-                    'variables_description' => $email['variables_description'],
-                    'type' => $email['type'],
-                    'created_at' => $now,
-                    'updated_at' => $now
-                ];
+                    $emails[] = [
+                        'key' => $key,
+                        'is_active' => $email['is_active'],
+                        'subject' => $email['subject'],
+                        'from' => env('MAIL_FROM', $from),
+                        'from_name' => $email['from_name'],
+                        'body' => $email['body'],
+                        'variables' => $email['variables'],
+                        'variables_description' => $email['variables_description'],
+                        'type' => $email['type'],
+                        'created_at' => $now,
+                        'updated_at' => $now
+                    ];
+                }
+                EmailContent::query()->upsert($emails, 'key');
+                cache(['wallet_email_contents' => $emails]);
             }
-            EmailContent::query()->upsert($emails,'key');
-            cache(['wallet_email_contents' => $emails]);
         }
     }
 

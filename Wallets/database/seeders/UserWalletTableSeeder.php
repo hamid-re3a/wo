a2a29328ad7d9d6caa7aa42afe140ccf5061c0d8
NimaN2D <nima.nouri.d@gmail.com>
2021-11-19 03:35:22 +0330

    Deposit to users wallet seeder improvements

--- Wallets/database/seeders/UserWalletTableSeeder.php
+++ Wallets/database/seeders/UserWalletTableSeeder.php
@@ -19,30 +19,25 @@ class UserWalletTableSeeder extends Seeder
     public function run()
     {
         $user = User::query()->firstOrCreate(['id' => 1]);
+
         $bankService = new BankService($user);
+
         $bankService->getWallet(WALLET_NAME_DEPOSIT_WALLET);
         $bankService->getWallet(WALLET_NAME_EARNING_WALLET);
-        $bankService->deposit(WALLET_NAME_DEPOSIT_WALLET,10000000);
-        if (!in_array(app()->environment(), ['production', 'staging'])) {
-            /**
-             *
-             */
-            $user = User::query()->firstOrCreate(['id' => 2]);
-            $bankService = new BankService($user);
-            $bankService->getWallet(WALLET_NAME_DEPOSIT_WALLET);
-            $bankService->getWallet(WALLET_NAME_EARNING_WALLET);
-            $bankService->deposit(WALLET_NAME_DEPOSIT_WALLET, 10000000000);
-            $bankService->deposit(WALLET_NAME_EARNING_WALLET, 10000000000);
 
-            /**
-             *
-             */
+        if (!in_array(app()->environment(), ['production', 'staging'])) {
+
             $customer_user = User::query()->firstOrCreate(['id' => 3]);
             $bankService = new BankService($customer_user);
+
             $bankService->getWallet(WALLET_NAME_DEPOSIT_WALLET);
             $bankService->getWallet(WALLET_NAME_EARNING_WALLET);
-            $bankService->deposit(WALLET_NAME_DEPOSIT_WALLET, 10000000000);
-            $bankService->deposit(WALLET_NAME_EARNING_WALLET, 10000000000);
+
+            if ($bankService->getWallet(WALLET_NAME_DEPOSIT_WALLET) == 0)
+                $bankService->deposit(WALLET_NAME_DEPOSIT_WALLET, 10000000000);
+
+            if ($bankService->getWallet(WALLET_NAME_EARNING_WALLET) == 0)
+                $bankService->deposit(WALLET_NAME_EARNING_WALLET, 10000000000);
 
 
         }
